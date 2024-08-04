02072d1254f11b17fb15ac78ea1a293da45eefb9
Andrew Hu <andrewyuhu02@gmail.com>
2024-08-03 20:45:33 -0700

    removing node_modules and changing start script

--- index.js
+++ index.js
@@ -32,4 +32,6 @@ app.post("/new", (req, res) => {
     res.redirect("/")
 });
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+const port = process.env.PORT | 3000;
+
+app.listen(port, () => console.log("Running app..."));
