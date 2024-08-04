22d18c70946bf8fc268bec785d35fbc23552d766
Andrew Hu <andrewyuhu02@gmail.com>
2024-08-03 20:47:25 -0700

    fixed a silly issue

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ app.post("/new", (req, res) => {
     res.redirect("/")
 });
 
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log("Running app..."));
